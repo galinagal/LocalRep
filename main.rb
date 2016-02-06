ad420046f5252be2afdd175d9cb1c5de8ca6657a
galinagal <galgalushka@gmail.com>
2016-02-06 12:54:01 +0200

    Main.rb modified

--- main.rb
+++ main.rb
@@ -1,10 +1,13 @@
 require "pry"
+ARGV.each do |var|
+  puts var
+end
 x_point, y_point, x, y= ARGV
-if x==x_point && y=y_point 
+if x_point==x && y_point==y 
    puts "Точка найдена"
-elsif x!=x_point && y!=y_point
+elsif x_point!=x && y_point!=y
   puts "Близко, но нет"
-elsif x==x_point && y!=y_point
+elsif y_point!=y && x==x_point
   puts "X координата верна, у - нет"
 elsif x!=x_point && y==y_point
  puts "Y координата верна, x - нет"
